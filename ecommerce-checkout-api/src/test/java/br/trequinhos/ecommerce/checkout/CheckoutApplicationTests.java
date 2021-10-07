bab91bb58057b28ce3e27a446d53a13f929908bb
Josué Alves de Oliveira <josueao.mobile@gmail.com>
2021-10-07 17:52:35 -0300

    Estrutura inicial checkout-api

---
+++ ecommerce-checkout-api/src/test/java/br/trequinhos/ecommerce/checkout/CheckoutApplicationTests.java
@@ -0,0 +1,13 @@
+package br.trequinhos.ecommerce.checkout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
